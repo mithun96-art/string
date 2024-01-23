80f60209a9199979758d849a426d42cd064d0b1c
mithun96-art <157538757+mithun96-art@users.noreply.github.com>
2024-01-23 16:29:13 -0500

    Add files via upload

---
+++ string.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 16 09:29:56 2023
+
+@author: mithun sai
+"""
+# this coding for string in python
+# this is python coding by mithun sai
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+print("Hello World")
+print('Hello World')
+a = "Hello"
+print(a)
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
